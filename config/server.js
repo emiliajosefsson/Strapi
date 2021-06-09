ce5ba3bdc6a7c2bca6799a81f54ef76f7a0a2159
emiliajosefsson <emilia.josefssons@gmail.com>
2021-06-09 09:43:11 +0200

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b80e29534464d4ee8b7c418225bbe07a'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '3a61d8a2-cbb0-4343-be01-3eb3dab26bd6'
+};
